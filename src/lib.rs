e7648f60d6fbe68285c70524f2450b0f6abd7c6f
Ebooth <pauldejeandev@gmail.com>
2025-02-25 00:54:47 +0100

    [Step 2] Added the feature to extract files from tarball

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     fs,
     io::{stdin, Read},
     str,
@@ -12,61 +13,153 @@ use clap::Parser;
 #[command(version = "1.0")]
 #[command(about = "A copy of unix command line tool tar", long_about = None)]
 pub struct Args {
-    #[arg(short = 't', long = "list")]
-    list: bool,
+    #[arg(short = 't', long = "list", group = "mode")]
+    pub list: bool,
+
+    #[arg(short = 'x', long = "extract", group = "mode")]
+    pub extract: bool,
 
     #[arg(short = 'f', long = "file")]
-    file: Option<String>,
+    pub file: Option<String>,
 }
 
 pub fn execute_command(args: &Args) -> i32 {
-    let data = match args
+    match launch(args) {
+        Ok(_) => 0,
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            1
+        }
+    }
+}
+
+fn launch(args: &Args) -> Result<(), Box<dyn std::error::Error>> {
+    if !args.list && !args.extract {
+        return Err("No mode specified".into());
+    }
+    let data = args
         .file
         .as_ref()
         .map(|value| read_tar_achive(value))
-        .unwrap_or_else(|| read_from_stdin())
-    {
-        Ok(data) => data,
-        Err(e) => {
-            eprintln!("Error: {}", e);
-            return 1;
-        }
-    };
+        .unwrap_or_else(|| read_from_stdin())?;
 
     let blocks: Vec<[u8; 512]> = data
         .chunks_exact(512)
         .map(|chunk| chunk.try_into().expect("Invalid chunk size"))
         .collect();
 
-    for block in blocks {
-        if is_valid_header(&block) {
-            let file_name = match extract_file_name(&block) {
-                Ok(name) => name,
-                Err(e) => {
-                    eprintln!("Error: {}", e);
-                    return 1;
-                }
-            };
+    if args.list {
+        let file_names = get_file_names(blocks)?;
+        for file_name in file_names {
             println!("{}", file_name);
         }
+    } else if args.extract {
+        let files = extract_files(blocks)?;
+        for (file_name, content) in files {
+            fs::write(&file_name, content)?;
+        }
     }
-    return 0;
+    return Ok(());
 }
 
-fn extract_file_name(header_block: &[u8; 512]) -> Result<String, std::io::Error> {
+fn extract_files(
+    blocks: Vec<[u8; 512]>,
+) -> Result<HashMap<String, Vec<u8>>, Box<dyn std::error::Error>> {
+    let mut files: HashMap<String, Vec<u8>> = HashMap::new();
+    let mut current_file: Option<String> = None;
+    let mut nb_bytes = 0;
+
+    for block in blocks {
+        if block.iter().all(|&x| x == 0) {
+            if let Some(ref file_name) = current_file {
+                truncate_file_content(file_name, &mut files, nb_bytes)?;
+            }
+        } else if is_valid_header(&block) {
+            if let Some(ref file_name) = current_file {
+                truncate_file_content(file_name, &mut files, nb_bytes)?;
+            }
+
+            let (file_name, content_size) = extract_file_info(&block)?;
+            current_file = Some(file_name);
+            nb_bytes = content_size;
+            files.insert(current_file.clone().expect("file not found"), vec![]);
+        } else {
+            let file_name = current_file.as_ref().ok_or("Error: No file name found")?;
+            let content = files
+                .get_mut(file_name.as_str())
+                .ok_or("File content not found")?;
+            content.extend_from_slice(&block);
+        }
+    }
+    return Ok(files);
+}
+
+fn truncate_file_content(
+    file_name: &str,
+    files: &mut HashMap<String, Vec<u8>>,
+    nb_bytes: usize,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let content = files.get_mut(file_name).ok_or("File content not found")?;
+    if content.len() < nb_bytes {
+        eprintln!("Error: Incomplete file");
+        return Err("Error extracting file content".into());
+    }
+    content.truncate(nb_bytes);
+
+    Ok(())
+}
+
+fn get_file_names(blocks: Vec<[u8; 512]>) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let mut file_names = Vec::new();
+    for block in blocks {
+        if is_valid_header(&block) {
+            let file_name = extract_file_name(&block)?;
+            file_names.push(file_name);
+        }
+    }
+    return Ok(file_names);
+}
+
+fn extract_file_info(
+    header_block: &[u8; 512],
+) -> Result<(String, usize), Box<dyn std::error::Error>> {
+    let file_name = extract_file_name(header_block)?;
+    let content_size = extract_content_size(header_block);
+    return Ok((file_name, content_size));
+}
+
+fn extract_file_name(header_block: &[u8; 512]) -> Result<String, Box<dyn std::error::Error>> {
     let file_name_field = &header_block[0..100];
     let file_name = match str::from_utf8(file_name_field) {
         Ok(s) => s
             .trim_matches(|c: char| c == '\0' || c.is_whitespace())
             .trim(),
         Err(_) => {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::InvalidData,
-                "Invalid file name",
-            ));
+            return Err("Error extracting file name".into());
         }
     };
     return Ok(file_name.to_string());
+}
+
+fn extract_content_size(header_block: &[u8; 512]) -> usize {
+    let size_field = &header_block[124..136];
+    let size_str = match str::from_utf8(size_field) {
+        Ok(s) => s
+            .trim_matches(|c: char| c == '\0' || c.is_whitespace())
+            .trim(),
+        Err(_) => {
+            return 0;
+        }
+    };
+
+    let size = match usize::from_str_radix(size_str, 8) {
+        Ok(num) => num,
+        Err(_) => {
+            return 0;
+        }
+    };
+
+    return size;
 }
 
 fn is_valid_header(block: &[u8; 512]) -> bool {
@@ -87,8 +180,7 @@ fn is_valid_header(block: &[u8; 512]) -> bool {
 
     let stored_checksum = match u32::from_str_radix(checksum_str, 8) {
         Ok(num) => num,
-        Err(e) => {
-            eprintln!("Error parsing checksum field as octal: {:?}", e);
+        Err(_) => {
             return false;
         }
     };
@@ -118,7 +210,7 @@ fn read_tar_achive(file: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     return Ok(data);
 }
 
-pub fn read_from_stdin() -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+fn read_from_stdin() -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     let mut content = Vec::new();
     stdin().lock().read_to_end(&mut content)?;
     Ok(content)
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -16,38 +16,39 @@ fn create_temp_file(dir: &TempDir, content: &str) -> NamedTempFile {
     return temp_file;
 }
 
-fn get_relative_path(file: &NamedTempFile) -> String {
-    let current_dir = env::current_dir().expect("error getting current directory");
-    return file
-        .path()
-        .strip_prefix(&current_dir)
-        .expect("error getting relative path")
-        .to_str()
-        .expect("error converting path to str")
-        .to_string();
-}
-
 #[test]
 fn should_decode_content_of_tar_from_stdin() {
     let dir = create_temp_dir();
     let content = "Hello World";
     let file = create_temp_file(&dir, content);
-    let file_path = &get_relative_path(&file);
-    let archive_name = &format!("{}/archive.tar", { dir.path().to_str().unwrap() });
+    let file_name = file
+        .path()
+        .file_name()
+        .expect("error extracting file name")
+        .to_str()
+        .expect("error converting file name to string");
+    let archive_name = "archive.tar";
+    let dir_path = dir
+        .path()
+        .to_str()
+        .expect("error converting dir path to string");
 
     ProcessCommand::new("tar")
-        .args(["-cf", archive_name, file_path])
+        .current_dir(dir.path())
+        .args(["-cf", archive_name, file_name])
         .output()
         .expect("error creating tar archive");
 
-    let archive_bytes = std::fs::read(&archive_name).expect("error reading tar archive from disk");
+    let archive_bytes = std::fs::read(format!("{dir_path}/{archive_name}"))
+        .expect("error reading tar archive from disk");
 
     let mut cmd = Command::cargo_bin("ctar").expect("error getting ctar binary");
     cmd.args(["-t"])
+        .current_dir(dir.path())
         .write_stdin(archive_bytes)
         .assert()
         .code(0)
-        .stdout(predicate::str::contains(file_path));
+        .stdout(predicate::str::contains(file_name));
 }
 
 #[test]
@@ -55,19 +56,26 @@ fn should_decode_content_of_tar_made_of_one_file() {
     let dir = create_temp_dir();
     let content = "Hello World";
     let file = create_temp_file(&dir, content);
-    let file_path = &get_relative_path(&file);
-    let archive_name = &format!("{}/archive.tar", { dir.path().to_str().unwrap() });
+    let file_name = file
+        .path()
+        .file_name()
+        .expect("error extracting file name")
+        .to_str()
+        .expect("error converting file name to string");
+    let archive_name = "archive.tar";
 
     ProcessCommand::new("tar")
-        .args(["-cf", archive_name, file_path])
+        .current_dir(dir.path())
+        .args(["-cf", archive_name, file_name])
         .output()
         .expect("error creating tar archive");
 
     let mut cmd = Command::cargo_bin("ctar").expect("error getting ctar binary");
     cmd.args(["-tf", archive_name])
+        .current_dir(dir.path())
         .assert()
         .code(0)
-        .stdout(predicate::str::contains(file_path));
+        .stdout(predicate::str::contains(file_name));
 }
 
 #[test]
@@ -75,21 +83,74 @@ fn should_decode_content_of_tar_made_of_two_file() {
     let dir = create_temp_dir();
     let file_content1 = "Hello World";
     let file1 = create_temp_file(&dir, file_content1);
-    let file_path1 = &get_relative_path(&file1);
+    let file_name1 = file1
+        .path()
+        .file_name()
+        .expect("error extracting file name")
+        .to_str()
+        .expect("error converting file name to string");
     let file_content2 = "I have a dream!";
     let file2 = create_temp_file(&dir, file_content2);
-    let file_path2 = &get_relative_path(&file2);
-    let archive_name = &format!("{}/archive.tar", { dir.path().to_str().unwrap() });
+    let file_name2 = file2
+        .path()
+        .file_name()
+        .expect("error extracting file name")
+        .to_str()
+        .expect("error converting file name to string");
+    let archive_name = "archive.tar";
 
     ProcessCommand::new("tar")
-        .args(["-cf", archive_name, file_path1, file_path2])
+        .current_dir(&dir)
+        .args(["-cf", archive_name, file_name1, file_name2])
         .output()
         .expect("error creating tar archive");
 
     let mut cmd = Command::cargo_bin("ctar").expect("error getting ctar binary");
 
     cmd.args(["-tf", archive_name])
+        .current_dir(dir.path())
         .assert()
         .code(0)
-        .stdout(predicate::str::contains(file_path1).and(predicate::str::contains(file_path2)));
+        .stdout(predicate::str::contains(file_name1).and(predicate::str::contains(file_name2)));
+}
+
+#[test]
+fn should_extract_file_from_tar() {
+    let dir = create_temp_dir();
+    let dir_path = dir
+        .path()
+        .to_str()
+        .expect("error converting dir path to string");
+    let file_content1 = "Hello World";
+    let file = create_temp_file(&dir, file_content1);
+
+    let archive_name = "archive.tar";
+
+    let file_name = file
+        .path()
+        .file_name()
+        .expect("error extracting file name")
+        .to_str()
+        .expect("error converting file name to string");
+
+    ProcessCommand::new("tar")
+        .current_dir(&dir)
+        .args(["-cf", archive_name, file_name])
+        .output()
+        .expect("error creating tar archive");
+
+    let dir2 = create_temp_dir();
+    let mut cmd = Command::cargo_bin("ctar").expect("error getting ctar binary");
+
+    cmd.args(["-xf", &format!("{dir_path}/{archive_name}")])
+        .current_dir(dir2.path())
+        .assert()
+        .code(0);
+
+    let extracted_file_path = dir2.path().join(&file_name);
+
+    let extracted_content =
+        std::fs::read_to_string(&extracted_file_path).expect("error reading extracted file");
+
+    assert_eq!(extracted_content, file_content1);
 }
